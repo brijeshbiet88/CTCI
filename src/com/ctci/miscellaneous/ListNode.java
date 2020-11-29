d1ae232f802f4763a13a3c6edcbb979e80bb14fa
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-11-29 20:56:13 +0530

    ListNode

---
+++ src/com/ctci/miscellaneous/ListNode.java
@@ -0,0 +1,24 @@
+package com.ctci.miscellaneous;
+
+public class ListNode {
+
+	int data;
+	ListNode next;
+	
+	public ListNode(int data) {
+		super();
+		this.data = data;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public ListNode getNext() {
+		return next;
+	}
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+}
