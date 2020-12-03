e21d5221b644b4f1c475f61d93e86865f5143301
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-12-03 23:52:47 +0530

    Level Order Traversal Of Tree Each Level In New Line

--- src/com/ctci/tree/LevelOrderTraversal.java
+++ src/com/ctci/tree/LevelOrderTraversal.java
@@ -9,13 +9,21 @@ public class LevelOrderTraversal {
 		TreeNode root = TreeUtils.get7NodeTree();
 		System.out.println("Level Order Traversal Of Tree:");
 		traverseLevelorder(root);
+		System.out.println();
+		System.out.println("Level Order Traversal Of Tree Each Level :");
+		traverseLevelorderNewLine(root);
 	}
 
-	private static void traverseLevelorder(TreeNode root) {
+	private static void traverseLevelorderNewLine(TreeNode root) {
+
 		Queue<TreeNode> q = new LinkedList<TreeNode>();
 		q.add(root);
-		
+		int n = 0;
 		while(!q.isEmpty()) {
+			n = q.size();
+			
+			while(n > 0) {
+			
 			TreeNode tmp = q.poll();
 			System.out.print(tmp.data+" ");
 			
@@ -26,9 +34,31 @@ public class LevelOrderTraversal {
 			if(tmp.right != null) {
 				q.add(tmp.right);
 			}
-			
+			n--;
+			}
+			System.out.println();
 		}
 		
+	}
+
+	private static void traverseLevelorder(TreeNode root) {
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		q.add(root);
+
+		while (!q.isEmpty()) {
+			TreeNode tmp = q.poll();
+			System.out.print(tmp.data + " ");
+
+			if (tmp.left != null) {
+				q.add(tmp.left);
+			}
+
+			if (tmp.right != null) {
+				q.add(tmp.right);
+			}
+
+		}
+
 	}
 
 }
@@ -36,4 +66,9 @@ public class LevelOrderTraversal {
 /*
 Level Order Traversal Of Tree:
 1 2 3 4 5 6 7 
+Level Order Traversal Of Tree Each Level :
+1 
+2 3 
+4 5 6 7 
+
 */
